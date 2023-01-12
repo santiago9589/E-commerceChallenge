6a92deea37d1c3b709331ad784501fa0ba2234fb
santiago9589 <santiagodejesus587@gmail.com>
2023-01-12 20:08:50 -0300

    tailwincss config done

---
+++ my-app/tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      'sm': '375px',
+      'xl': '1440px',
+    },
+    extend:
+    {
+      colors: {
+        primary: {
+          500: 'hsl(26, 100%, 55%)',
+        }
+      },
+      fontFamily: {
+        'KumbhSans': ['Kumbh Sans', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
