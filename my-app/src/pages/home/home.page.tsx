c8160a45cbfb03e5238bdbde05574c33e1d7e867
santiago9589 <santiagodejesus587@gmail.com>
2023-01-12 23:45:56 -0300

    ñeft  hand working

--- my-app/src/pages/home/home.page.tsx
+++ my-app/src/pages/home/home.page.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import NavbarComponent from '../../components/NavbarComponent.components'
 import SubtitleComponent from '../../components/SubtitleComponent'
 import TitleComponent from '../../components/TitleComponent'
+import productImagen1 from "../../assets/image-product-1.jpg"
 
 const HomePage = () => {
   return (
     <>
       <NavbarComponent />
       <section className='flex h-5/6 p-4 box-border gap-6 w-full'>
-        <section className='flex-1 border-2 border-red-500 p-4 box-border '>
-          <img src="" />
-
+        <section className='flex-1  border-red-500 p-4 pt-12 box-border flex justify-center border-2 '>
+          
+            <img src={productImagen1} alt="product" className='w-2/3 h-2/3 rounded-lg'/>
+        
         </section>
         <section className='flex-1 border-2 border-red-500 flex flex-col justify-center items-start p-8'>
           <SubtitleComponent subtitle='Sneaker Company' />
@@ -28,7 +30,6 @@ const HomePage = () => {
               </section>
               <p className='opacity-50  line-through'>$250.00 </p>
             </section>
-
           </section>
           <section className='w-full flex items-start space-x-2 mt-4'>
             <section className='bg-slate-50 rounded-lg p-2 w-1/3 border-2 border-red-500 flex items-center justify-around'>
@@ -40,40 +41,9 @@ const HomePage = () => {
           </section>
         </section>
       </section>
-      {/* <section className='grid grid-cols-2x w-screen grid-rows-2x gap-4 mt-4'>
-        <section className='border-2 border-red-400 flex justify-center items-center'>
-          dsfsdf
-        </section>
-        <section className='border-2 border-red-400 flex flex-col justify-center items-start'>
-          
-        </section>
-      </section> */}
     </>
   )
 }
 
 export default HomePage
 
-{/* <section className='w-1/2 mx-auto'>
-            <SubtitleComponent subtitle='Sneaker Company' />
-            <TitleComponent title='Fall Limited Edition Sneakers' />
-            <p className='opacity-70'>
-              These low-profile sneakers are your perfect casual wear companion.
-              Featuring a durable rubber outer sole
-              ,they’ll withstand everything the weather can offer
-            </p>
-            <section>
-              <section className='flex'>
-                <p>$125.00</p><p>50%</p>
-              </section>
-              <p>$250.00 </p>
-            </section>
-            <section>
-              <>
-                <button>-</button>0<button>+</button>
-              </>
-              <>
-                <button>Add to Card</button>
-              </>
-            </section>
-          </section> */}
